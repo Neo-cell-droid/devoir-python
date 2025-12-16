a3ae4b3b930448efcc908788b03f1f3606b67256
Issam Lastab <i.lastab69@gmail.com>
2025-12-16 15:38:05 +0100

    Ajoute la page de calcul de somme avec affichage du résultat (j'ai testé avec 5 et 3)

--- app.py
+++ app.py
@@ -12,6 +12,18 @@ def index():
 def bonjour(nom):
     return render_template("bonjour.html", nom=nom)
 
+@app.route("/somme/<int:a>/<int:b>")
+def somme(a, b):
+    resultat = a + b
+    if resultat > 0:
+        texte = "positif"
+    elif resultat < 0:
+        texte = "négatif"
+    else:
+        texte = "nul"
+    return render_template("somme.html", a=a, b=b, resultat=resultat, texte=texte)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
