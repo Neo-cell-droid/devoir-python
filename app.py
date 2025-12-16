924fe72f7c7887887c8b28dc7298d1acfe78fbf3
Issam Lastab <i.lastab69@gmail.com>
2025-12-16 15:27:42 +0100

    Ajoute la page de salutation dynamique avec paramètre dans l'URL

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__)  
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/bonjour/<nom>")
+def bonjour(nom):
+    return render_template("bonjour.html", nom=nom)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
